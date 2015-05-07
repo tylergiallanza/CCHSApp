ed51d06c868adce75ee15f7035794ac3e9f510b6
atsalyuk <student@cch-w431-imac09.ccsd.d5>
2015-05-07 08:01:44 -0600

    clubs view setup ish

--- CCHS App/CCHS App/ClubViewController.h
+++ CCHS App/CCHS App/ClubViewController.h
@@ -8,6 +8,8 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ClubViewController : UIViewController
+@interface ClubViewController : UITableViewController
+
+
 
 @end
--- CCHS App/CCHS App/ClubViewController.m
+++ CCHS App/CCHS App/ClubViewController.m
@@ -10,6 +10,8 @@
 
 @interface ClubViewController ()
 
+@property NSMutableArray* clubs;
+
 @end
 
 @implementation ClubViewController
@@ -17,13 +19,57 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     NSLog(@"club");
+    
+    _clubs = [[NSMutableArray alloc] init];
+    //for(int i=0; i<10; i++){
+        // lis of clubs?
+    //}
+    [_clubs addObject:@"DECA"];
+    [_clubs addObject:@"Diversty Task Force"];
+    [_clubs addObject:@"FCLA"];
+    [_clubs addObject:@"Club 4"];
+    [_clubs addObject:@"Club 5"];
+    
+    
+    UIBarButtonItem *b = [[UIBarButtonItem alloc] initWithTitle:@"Back" style: UIBarStyleDefault target: self action: @selector(back)];
+    self.navigationItem.leftBarButtonItem = b;
+                          
     // Do any additional setup after loading the view.
 }
 
+                          
+-(IBAction)back:(id)sender{
+    [self dismissViewControllerAnimated:YES completion: nil];
+    
+
+}
+
+                    
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+
+    // Return the number of rows in the section.
+    return _clubs.count;
+}
+
+
+//- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+//    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"reuseIdentifier" forIndexPath:indexPath];
+//    
+//    // Configure the cell...
+//    
+//    return cell;
+//}
+
 
 /*
 #pragma mark - Navigation
