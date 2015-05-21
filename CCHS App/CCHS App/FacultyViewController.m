e1a4e3812c698a0120105c95d248220f45ba468d
kmancini <student@cch-w431-imac11.ccsd.d5>
2015-05-21 08:00:43 -0600

    commenting!! yay

--- CCHS App/CCHS App/FacultyViewController.m
+++ CCHS App/CCHS App/FacultyViewController.m
@@ -2,7 +2,7 @@
 //  FacultyViewController.m
 //  CCHS App
 //
-//  Created by Student on 5/6/15.
+//  Created by Avi on 5/6/15.
 //  Copyright (c) 2015 Students. All rights reserved.
 //
 
--- CCHS App/CCHS App/club.h
+++ CCHS App/CCHS App/club.h
@@ -2,7 +2,7 @@
 //  club.h
 //  CCHS App
 //
-//  Created by Student on 5/12/15.
+//  Created by Katie on 5/12/15.
 //  Copyright (c) 2015 Students. All rights reserved.
 //
 
