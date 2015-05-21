f38f96c7fb5e1b678d21902170455ca2d5ed2c92
aswartz <avi_swartz@yahoo.com>
2015-05-21 09:06:43 -0600

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 034fbeea7c9ba5126447950055cb6585fb6a2e3e, reversing
    changes made to c06be4258b7e87dee5ebdfc77026dbe3cf2c6190.

--- CCHS App/CCHS App/club.m
+++ CCHS App/CCHS App/club.m
@@ -2,7 +2,7 @@
 //  club.m
 //  CCHS App
 //
-//  Created by Katie on 5/12/15.
+//  Created by Student on 5/12/15.
 //  Copyright (c) 2015 Students. All rights reserved.
 //
 
@@ -10,11 +10,11 @@
 
 @interface club ()
 
-@property NSArray *sponsers;//the names of all of the sponsers
-@property NSString *name;//the name of the club
-@property NSString *meetingDay;//the day(s) of the week in which the club meets
-@property NSString *meetingTime;//the time at which the club meets(preferably in the format start to end pm/am although some clubs didnt have an end time)
-@property NSString *meetingPlace;//the room in which the club meets
+@property NSArray *sponsers;
+@property NSString *name;
+@property NSString *meetingDay;
+@property NSString *meetingTime;
+@property NSString *meetingPlace;
 //@property NSArray *nicknames;
 
 
@@ -25,15 +25,6 @@
 
 @implementation club
 
-
-/**
- *initializing the club and adding the information in the form of params to the club
- * n- String format of the club name
- * t - string format of the time at which the club meets
- * day - string format of the day(s) on which the club meets
- * place - the string format of the room in which the club meets
- * spon - the array of all of the sponsers
- */
 - (id)initWithDetails: (NSString*) n and: (NSString*) t and: (NSString*) day and: (NSString*) place and: (NSArray*) spon{
     self = [super init];
     if(self){
@@ -49,46 +40,28 @@
     return self;
 }
 
-/**
- *returns the string of the name of the club
- *
- */
 -(NSString *) getName{
     return _name;
 }
-/**
- *returns a string of the tome at which the club meets
- *
- */
+
 -(NSString *) getTime{
     return _meetingTime;
 }
-
-/**
- *
- *returns a string of the day(s) on which the club meets
- */
 -(NSString *) getDay{
     return _meetingDay;
 }
-
-/**
- *returns an array of the sponsers
- *
- */
 -(NSArray *) getSponsers{
     return _sponsers;
 }
 
-/**
- * returns a string of the place at which the meeting is held
- *
- */
 -(NSString *) getPlace{
     return _meetingPlace;
 }
 
 
+-(void) setTimeNumbers: (long*) hour and: (long*) min{
+    _meetingTime = [NSString stringWithFormat: @"%l:%l", hour, min ];
+}
 
 
 
